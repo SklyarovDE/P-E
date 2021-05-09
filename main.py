1d0c181ca1426f57a81b13cc096ed7838ff65019
Sklyarov.D.E <Sklyarov.D.E@gmail.com>
2021-05-09 21:49:46 +0300

    add async

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import asyncio
 
+import aiohttp
 
-async def main(event_loop):
-    await event_loop.run_until_complete()
+
+async def main():
+    with aiohttp.ClientSession() as session:
+        r = await session.get("https://google.com")
+        html = await r.text()
+        print(html)
 
 if __name__ == '__main__':
     e = asyncio.get_event_loop()
-    print("eeee")
+    tasks = []
+    for i in range(0, 10):
+        tasks.append(main())
+    t = asyncio.gather(*tasks)
+    t.run_until_complite()
+
