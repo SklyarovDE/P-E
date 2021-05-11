2a1178e4fee8c7aa27c5051d8b334633d638fbdb
Sklyarov.D.E <Sklyarov.D.E@gmail.com>
2021-05-12 00:33:23 +0300

    test of BS

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-import asyncio
+import pandas as pd
+import PyPDF2
+import re
 
-import aiohttp
+# open the pdf file
+object = PyPDF2.PdfFileReader("/Users/dmitrysklyarov/Folder/Python/_10-K-2020-(As-Filed).pdf")
 
+# get number of pages
+NumPages = object.getNumPages()
 
-async def main():
-    with aiohttp.ClientSession() as session:
-        r = await session.get("https://google.com")
-        html = await r.text()
-        print(html)
+# define keyterms
+String = "Social"
 
-if __name__ == '__main__':
-    e = asyncio.get_event_loop()
-    tasks = []
-    for i in range(0, 10):
-        tasks.append(main())
-    t = asyncio.gather(*tasks)
-    t.run_until_complite()
-
+for i in range(0, NumPages):
+    PageObj = object.getPage(i)
+    print("this is page " + str(i))
+    Text = PageObj.extractText()
+    # print(Text)
+    ResSearch = re.search(String, Text)
+    print(ResSearch)
